25bf4aa016c9e5988136537e3498dce4e7fad40b
Abiiarm <abiarmansyah.2021@student.uny.ac.id>
2023-12-25 00:46:43 +0700

    Balik versi Awal

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,32 +1,16 @@
-// auth.js
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import githubAuth from "next-auth/providers/github";
 
-export default NextAuth({
+export const authOption = {
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    githubAuth({
+      clientId: process.env.GITHUB_CLIENT,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
-    // Add other providers as needed
   ],
-  callbacks: {
-    async jwt(token, user) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session(session, token) {
-      session.user.id = token.id;
-      return session;
-    },
-  },
-  pages: {
-    signIn: "/auth/signin",
-    signOut: "/auth/signout",
-    error: "/auth/error",
-    verifyRequest: "/auth/verify-request",
-    newUser: null,
-  },
-});
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOption);
+
+export { handler as GET, handler as POST };
